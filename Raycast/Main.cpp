d8ac15e91458a7ccb92629b263e634a863bee808
Ruairi Delahunt <ruairidelahunt@gmail.com>
2025-02-28 16:11:22 -0800

    Real Init

---
+++ Raycast/Main.cpp
@@ -0,0 +1,26 @@
+﻿#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+#include "Map.H"
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 800, 450 }), "Raycaster");
+
+	Map map(32.0f, 20, 20);
+
+    while (window.isOpen()) {
+        while (auto event = window.pollEvent()) {  // 
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        window.clear();
+		map.draw(window);
+        window.display();
+    }
+
+    return 0;
+}
